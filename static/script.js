95c54427bb167bb219f8294b0e76adb3e90750a0
lovustig <love.lustig@runbox.com>
2021-12-16 13:23:59 +0100

    script.js: add historyArray to keep track and remove duplicate images.

--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@
     function ajax(path, callback) {
         var request = new XMLHttpRequest()
         request.open('GET', path, true)
-        
+
         request.onreadystatechange = function() {
             if(request.readyState === XMLHttpRequest.DONE) {
                 var json_response = ''
@@ -16,11 +16,12 @@
         }
         request.send()
     }
-    
+
     function init() {
         var q = document.getElementById('question')
         var a = document.getElementById('answer')
         var o = document.getElementById('overlay')
+        var historyArray = []
 
         function getNext() {
             if(!o.classList.contains('hidden')) {
@@ -28,12 +29,19 @@
             }
             ajax('./app/random', function(json_result) {
                 q.dataset.src = json_result.picture
+                if (historyArray.length > 50) {
+                    historyArray = historyArray.shift()
+                    }
+                if (historyArray.includes(q.dataset.src)) {
+                    getNext()
+                    }
+                historyArray.push(q.dataset.src)
                 q.src = '/pictures/' + q.dataset.src
             })
         }
 
         getNext()
-        
+
         q.addEventListener('click', function reveal(e) {
             ajax('./app/solution/' + q.dataset.src, function(json_response) {
                 a.textContent = json_response.solution
@@ -46,7 +54,7 @@
         o.addEventListener('click', function next(e) {
             getNext()
         })
-        
+
     }
     document.addEventListener('DOMContentLoaded', init)
 })()
